ad841c6590e5018cd2a9e5e9dc914d9af50a985e
Joseph Ryan <JRYANGOLF@GMAIL.COM>
2020-05-12 22:20:03 -0500

    binarysearch

--- binarysearch.js
+++ binarysearch.js
@@ -1,5 +1,22 @@
-function binarysearch() {
-    
+function binarysearch(value, arr) {
+    var high = list.length - 1;
+    var low = 0;
+    var mid = 0;
+    for (i=0; low <= high; i++) {
+        mid = math.floor((high + low) /2)
+            if(arr[mid == value]{
+                return arr[mid];
+            }
+            else if (value > arr[mid]){
+                //move the low up one 
+                low = mid + 1;
+            }
+            else {
+                //move the high down to middle minus 1 and search the left half
+                high = mid -1;
+            }
+    }
+    return "not found"
 }
 
 
